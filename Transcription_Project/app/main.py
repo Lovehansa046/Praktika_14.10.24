1db2f4dc79daf8c68ed180ab4ca37534b62955ce
nazarigue <54268149+nazarigue@users.noreply.github.com>
2024-10-15 16:29:10 +0200

    Update main.py

--- Transcription_Project/app/main.py
+++ Transcription_Project/app/main.py
@@ -5,16 +5,20 @@ import os
 app = FastAPI()
 
 # Настройка MinIO
-minio_client = Minio(
-    os.getenv("MINIO_URL", "localhost:9000").replace("http://", "").replace("https://", ""),
-    access_key=os.getenv("MINIO_ACCESS_KEY", "minioadmin"),
-    secret_key=os.getenv("MINIO_SECRET_KEY", "minioadmin"),
-    secure=False
-)
 
-bucket_name = "mybucket"
-if not minio_client.bucket_exists(bucket_name):
-    minio_client.make_bucket(bucket_name)
+def get_minio_connector():
+    minio_client = Minio(
+        "minio:9000",
+        access_key="Root123",
+        secret_key="Root123312213n",
+        secure=False
+    )
+
+    bucket_name = "mybucket"
+    if not minio_client.bucket_exists(bucket_name):
+        minio_client.make_bucket(bucket_name)
+    
+    return minio_client, bucket_name
 
 # Эндпоинт для загрузки файла
 @app.post("/upload")
@@ -23,10 +27,14 @@ async def upload_file(file: UploadFile = File(...)):
         # Получаем данные файла
         file_data = await file.read()
         
+        # Получаем MinIO клиент и имя бакета
+        minio_client, bucket_name = get_minio_connector()
+        
         # Сохранение файла в MinIO
         minio_client.put_object(bucket_name, file.filename, file_data, length=len(file_data))
 
         return {"status": "File uploaded", "file_name": file.filename}
     
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred while uploading the file: {str(e)}")
+        print(e)
+        return str(e)
