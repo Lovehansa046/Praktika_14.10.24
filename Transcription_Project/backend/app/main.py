6a0444853a9294f8b33a4c2f0f800ca17df1d972
nazarigue <54268149+nazarigue@users.noreply.github.com>
2024-10-25 13:24:25 +0200

    Update main.py

--- Transcription_Project/backend/app/main.py
+++ Transcription_Project/backend/app/main.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI, UploadFile, File, Depends, HTTPException
 from sqlalchemy.orm import Session
 from starlette.middleware.cors import CORSMiddleware
-
+from utils import get_minio_connector,get_bucket_name, upload_file_to_minio,save_file_metadata_to_database
 from database import get_db, DBFiles  # Make sure to import your database utility functions
-from minio import Minio
+
 import os
-import io
+
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
@@ -20,58 +20,22 @@ app.add_middleware(
     allow_headers=["*"],
 )
 # Настройка MinIO
-def get_minio_connector():
-    minio_client = Minio(
-        "minio:9000",
-        access_key="Root123",
-        secret_key="Root123312213n",
-        secure=False
-    )
 
-    bucket_name = "mybucket"
-    if not minio_client.bucket_exists(bucket_name):
-        minio_client.make_bucket(bucket_name)
-
-    return minio_client, bucket_name
 
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
     try:
-        # Получаем MinIO клиент и имя бакета
-        minio_client, bucket_name = get_minio_connector()
 
-        # Читаем содержимое файла
         file_data = await file.read()
-        file_size = len(file_data)
-
-        # Создаем поток из прочитанных данных
-        file_stream = io.BytesIO(file_data)
-
-        # Сохранение файла в MinIO
-        minio_client.put_object(
-            bucket_name,
-            file.filename,
-            data=file_stream,
-            length=file_size
-        )
-
-        # Создаем объект для записи в базу данных
-        db_file = DBFiles(
-            title=file.filename,
-            size=file_size,
-            path=f"{bucket_name}/{file.filename}"
-        )
-
-        # Записываем информацию о файле в базу данных
-        db.add(db_file)
-        db.commit()
-        db.refresh(db_file)
-
-        return {"status": "File uploaded", "file_name": file.filename}
+        upload_result=upload_file_to_minio(file=file_data)
+        if upload_result:
+            save_file_metadata_to_database(file_data)
+            return {"status":"SUCCEEDED","message": "File uploaded", "file_name": file.filename}
+        else:
+            return {"status":"FAILED","message": "File upload failed", "file_name": file.filename}
 
     except Exception as e:
-        print(e)
         return {"status": "error", "message": str(e)}
 
 @app.get("/files")
